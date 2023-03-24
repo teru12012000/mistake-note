7cf940b4d57a122a4aff9f47bd99bded544e93ef
teru12012000 <sion0012@outlook.jp>
2023-03-24 16:17:37 +0900

    add login

---
+++ pages/Home/post/English.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const English:NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>ログイン</title>
+        <meta name="description" content="ログインのページです" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
+
+export default English;
