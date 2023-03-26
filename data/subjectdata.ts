52c459faab0b7b9db509cf6216f06705e4925196
teru12012000 <sion0012@outlook.jp>
2023-03-27 00:00:48 +0900

    一旦ここでセーブ

---
+++ data/subjectdata.ts
@@ -0,0 +1,18 @@
+
+export type list={
+  question:string;
+  questionlink:string[];
+  deffanswer:string;
+  deffanswerlink:string[];
+  realanswer:string;
+  realanswerlink:string[];
+  other:string;
+  otherlink:string[];
+  id:string;
+}
+export type imglink2={
+  questionlink:string[];
+  deffanswerlink:string[];
+  realanswerlink:string[];
+  otherlink:string[];
+}
